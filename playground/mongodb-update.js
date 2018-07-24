75f4bd3679084574ff5a916f0d02b59ec99da196
sunneyman <sunney-muz@ukr.net>
2018-07-24 12:24:37 +0300

    Add updatee file

---
+++ playground/mongodb-update.js
@@ -0,0 +1,30 @@
+// const MongoClient = require('mongodb').MongoClient;
+const {MongoClient, ObjectID} = require('mongodb');
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, client) => {
+    if (err) {
+        return console.log('Unable to connect to MongoDB server');
+    }
+    console.log('Connected to MongoDB server');    
+    const db = client.db('TodoApp');
+
+    db.collection('Users').findOneAndUpdate({
+        _id: new ObjectID('5b5617c21815826470af0f9d')
+    }, {
+       $set: {
+           name: 'Andrew'
+        },
+    //    $unset: {
+    //     lastName: ''    
+    //    },
+       $inc: {
+           age: -2
+       } 
+    }, {
+        returnOriginal: false
+    }).then((result) => {
+        console.log(result);
+    });
+    
+    client.close();
+});
