9f226b29c85931a76291b2b8d71c9e8d641b4a58
sunneyman <sunney-muz@ukr.net>
2018-07-25 09:48:34 +0300

    Add GET /todos route

--- server/server.js
+++ server/server.js
@@ -37,7 +37,21 @@ app.post('/users', (req, res) => {
     });
 });
 
+app.get('/todos', (req, res) => {
+    Todo.find().then((todos) => {
+        res.send({todos});
+    }, (e) => {
+        res.status(400).send(e);
+    });
+});
 
+app.get('/users', (req, res) => {
+    User.find().then((users) => {
+        res.send({users});
+    }, (e) => {
+        res.status(400).send(e);
+    });
+});
 
 app.listen(3000, () => {
     console.log('Started on port 3000');
