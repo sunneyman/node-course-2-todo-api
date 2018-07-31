3714d3275dcd2c0d06e8295ad9465a1f6528950b
sunneyman <sunney-muz@ukr.net>
2018-07-31 10:44:33 +0300

    Add test for GET  /users/me and POST /users

--- server/models/user.js
+++ server/models/user.js
@@ -38,7 +38,7 @@ UserSchema.methods.toJSON = function () {
     var user = this;
     var userObject = user.toObject();
     
-    return _.pick(userObject, ['_id', 'email', 'password']);
+    return _.pick(userObject, ['_id', 'email']);
 };
 
 UserSchema.methods.generateAuthToken = function () {
